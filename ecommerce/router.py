26d942137db7b27aa5f2ab2d5120c3bf3ec45427
Gregorio Canepa <gcanepa@mobydigital.com>
2021-04-24 18:18:58 -0300

    procfile added and took out unnecessary prints in routers

--- ecommerce/router.py
+++ ecommerce/router.py
@@ -7,6 +7,3 @@ router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
 router.register('orders', OrderViewSet, basename='order')
 router.register('orders_details', OrderDetailViewSet, basename='order_detail')
-
-for url in router.urls:
-    print(url)
