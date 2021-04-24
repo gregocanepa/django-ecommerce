d0f812fe0eeef81ace3955c4db00f60f39ab45f8
Gregorio Canepa <gcanepa@mobydigital.com>
2021-04-24 18:15:39 -0300

    requirement.txt and runtime for heroku, new basenames for urls in router file

--- ecommerce/router.py
+++ ecommerce/router.py
@@ -1,11 +1,12 @@
 from orders.views import ProductViewSet, OrderViewSet, OrderDetailViewSet
 from rest_framework import routers
+from django.urls import resolve
 
 router = routers.DefaultRouter()
 
-router.register('products', ProductViewSet)
-router.register('orders', OrderViewSet)
-router.register('orders_details', OrderDetailViewSet)
+router.register('products', ProductViewSet, basename='product')
+router.register('orders', OrderViewSet, basename='order')
+router.register('orders_details', OrderDetailViewSet, basename='order_detail')
 
 for url in router.urls:
     print(url)
